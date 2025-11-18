a69f27ad26b72ea43f3870b7ca812d897451132e
Pablo Isaac Sponchiado da Silva <pablostz2019@gmail.com>
2025-11-18 16:22:38 -0300

    Delete BACK-END/contatos-api directory

--- BACK-END/contatos-api/src/interface/ContatoDTO.ts
+++
@@ -1,8 +0,0 @@
-export interface ContatoDTO {
-  id_contato?: string, // ID do cliente (? indica que é opcional)
-  nome_completo: string, // Nome do cliente
-  telefone: number, // Email do cliente
-  data_nascimento?: number; // Data de nascimento do cliente
-  email?: string; // Email do cliente
-  observacoes?: string; // Observações adicionais sobre o cliente
-}
